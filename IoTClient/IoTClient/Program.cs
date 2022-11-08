cae4b36ca4e696921c80238d3dbbb1e3eaa49176
YunGuGu99 <116170611+YunGuGu99@users.noreply.github.com>
2022-11-08 20:57:09 +0900

    Add files via upload

---
+++ IoTClient/IoTClient/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Timers;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft;
+
+namespace IoTClient
+{
+    internal class Program
+    {
+        private static System.Timers.Timer SensorTimer;
+        private const string DeviceConnectionString = "";
+        private const string DeviceID = "Device1";
+        private static DeviceClient SensorDevice = null;
+
+        private static DummySensor DummySensor = new DummySensor();
+        static void Main(string[] args)
+        {
+            SetTimer();
+
+            SensorDevice = DeviceClient.CreateFromConnectionString(DeviceConnectionString, DeviceID);
+
+            if (SensorDevice == null)
+            {
+                Console.WriteLine("Failed to create DeviceClient!");
+                SensorTimer.Stop();
+            }
+
+            Console.WriteLine("\nPress Enter Key to exit the application...\n");
+            Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+            Console.ReadLine();
+            SensorTimer.Stop();
+            SensorTimer.Dispose();
+        }
+
+        private static void SetTimer()
+        {
+            SensorTimer = new System.Timers.Timer(2000);
+            SensorTimer.Elapsed += SensorTimer_Elapsed;
+            SensorTimer.AutoReset = true;
+            SensorTimer.Enabled = true;
+        }
+
+        private static void SensorTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}", e.SignalTime);
+            SendEvent();
+        }
+
+        private static void SendEvent()
+        {
+            WeatherModel model = DummySensor.GetWeatherModel(DeviceID);
+
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(model);
+
+            Console.WriteLine(json);
+        }
+    }
+}
